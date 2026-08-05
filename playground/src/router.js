239992e7d3ea33330f1ed1a118c636fa80c77584
Konstantyn Alyohyn <48721832+KosAlyohyn@users.noreply.github.com>
2026-08-05 12:24:15 +0300

    Set public URL

--- playground/src/router.js
+++ playground/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import BooleanFilters from './pages/BooleanFilters.vue'
 import CustomParams from './pages/CustomParams.vue'
@@ -11,7 +11,7 @@ import SearchFilters from './pages/SearchFilters.vue'
 import TagFilters from './pages/TagFilters.vue'
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
